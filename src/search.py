e9decc0ee7349553f4e5dfe99a873bf3cf6380e7
Alvie Rahman <alvierahman90@gmail.com>
2021-02-25 21:30:45 +0000

    clear repo

--- src/search.py
+++
@@ -1,8 +0,0 @@
-from fuzzywuzzy import process
-
-choices = open('tmp').read().split('\n')
-
-search_term = input('Search term: ')
-
-for result, matchness in process.extract(search_term, choices):
-    print(str(matchness), result)
